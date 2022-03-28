af128b5f312cb81c122feb69d3d48a4fa805c02e
Tony Habib <thabib@ndu.edu.lb>
2022-03-28 08:50:40 +0300

    finalize the assignment 2

--- index.php
+++ index.php
@@ -1,3 +1,71 @@
+<?php
+// Change the order in the header in order to swap the columns
+$headers = [
+    ["key" => "lastName", "label" => "Last Name"],
+    ["key" => "firstName", "label" => "First Name"],
+    ["key" => "email", "label" => "Email"],
+    ["key" => "age", "label" => "Age"]
+];
+# loop over the headers, then grab they key, and access the key in the body loop variable
+#first iteraton => row = [] => loop over the headers as header => header["key"] = firstName, lastName, email, age
+$body = [
+    [
+        "firstName" => ["value" => "Tony", "color" => "red"],
+        "lastName" => ["value" => "Habib", "color" => "magenta"],
+        "email" => ["value" => "thabib@ndu.edu.lb", "color" => "blue"],
+        "age" => ["value" => "28", "color" => "green"],
+    ],
+    [
+        "firstName" => ["value" => "Elias", "color" => "magenta"],
+        "lastName" => ["value" => "Damoury", "color" => "blue"],
+        "email" => ["value" => "edamoury@ndu.edu.lb", "color" => "green"],
+        "age" => ["value" => "21", "color" => "red"],
+    ],
+    [
+        "firstName" => ["value" => "Bassel", "color" => "blue"],
+        "lastName" => ["value" => "Succar", "color" => "green"],
+        "email" => ["value" => "bsuccar@ndu.edu.lb", "color" => "magenta"],
+        "age" => ["value" => "20", "color" => "red"],
+    ],
+    [
+        "firstName" => ["value" => "Marilyne", "color" => "green"],
+        "lastName" => ["value" => "Nakad", "color" => "magenta"],
+        "email" => ["value" => "mnakad@ndu.edu.lb", "color" => "red"],
+        "age" => ["value" => "19", "color" => "blue"],
+    ],
+    [
+        "firstName" => ["value" => "Paul", "color" => "magenta"],
+        "lastName" => ["value" => "Araj", "color" => "green"],
+        "email" => ["value" => "paraj@ndu.edu.lb", "color" => "blue"],
+        "age" => ["value" => "55", "color" => "red"],
+    ]
+];
+
+function print_headers($headers)
+{
+    print "<tr>";
+    foreach ($headers as $header) {
+        print "<th>{$header['label']}</th>";
+    }
+    print "</tr>";
+}
+
+function print_table($headers, $body)
+{
+    print_headers($headers);
+
+    foreach ($body as $row) {
+        print "<tr>";
+        foreach ($headers as $header) {
+            print "<td style='color: {$row[$header["key"]]["color"]}'>{$row[$header["key"]]["value"]}</td>";
+
+        }
+        print "</tr>";
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,31 +74,7 @@
 </head>
 <body>
 <table>
-    <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Savings</th>
-    </tr>
-    <tr>
-        <td>Peter</td>
-        <td>Griffin</td>
-        <td>$100</td>
-    </tr>
-    <tr>
-        <td>Lois</td>
-        <td>Griffin</td>
-        <td>$150</td>
-    </tr>
-    <tr>
-        <td>Joe</td>
-        <td>Swanson</td>
-        <td>$300</td>
-    </tr>
-    <tr>
-        <td>Cleveland</td>
-        <td>Brown</td>
-        <td>$250</td>
-    </tr>
+    <?php print_table($headers, $body) ?>
 </table>
 
 </body>
